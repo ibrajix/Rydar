23182c8f3b015f73eb2002c3d4277bda3191d20a
ibrajix <ibrajix@gmail.com>
2021-12-06 00:01:12 +0100

    Started implementing persistent bottom sheet

--- app/src/main/java/com/ibrajix/rydar/ui/activities/ContainerActivity.kt
+++ app/src/main/java/com/ibrajix/rydar/ui/activities/ContainerActivity.kt
@@ -3,20 +3,14 @@ package com.ibrajix.rydar.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ibrajix.rydar.R
-import com.ibrajix.rydar.ui.fragments.ConfirmBottomDialogSheetFragment
 import com.ibrajix.rydar.utils.GeneralUtility.transparentStatusBar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ContainerActivity : AppCompatActivity(), ConfirmBottomDialogSheetFragment.ItemClickListener {
+class ContainerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         transparentStatusBar()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_container)
     }
-
-    override fun onItemClick(item: String) {
-        //respond to item click
-    }
-
 }
--- app/src/main/java/com/ibrajix/rydar/ui/fragments/ConfirmDestinationFragment.kt
+++ app/src/main/java/com/ibrajix/rydar/ui/fragments/ConfirmDestinationFragment.kt
@@ -5,15 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.ibrajix.rydar.R
 import com.ibrajix.rydar.databinding.FragmentConfirmDestinationBinding
 import com.ibrajix.rydar.databinding.FragmentHomeBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 
 
 class ConfirmDestinationFragment : Fragment() {
 
     private var _binding: FragmentConfirmDestinationBinding? = null
     private val binding get() = _binding!!
+    private var mBottomSheetLayout: ConstraintLayout? = null
+    private var sheetBehavior: BottomSheetBehavior<*>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
